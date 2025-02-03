50cf7b371c0594885187786d6efeed287c0d7371
c0z <thelastasylum@gmail.com>
2025-02-03 13:55:45 -0500

    Updated havoc_arm.sh
    Cleaned up function names, organized code, de-duplicated script sections and standardized script.

--- havoc_arm.sh
+++ havoc_arm.sh
@@ -1,60 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 ENDCOLOR='\033[0m'
 
- echo -e "${BLUE}_  _   ___   _____   ___   ___ ___  ___     _   ___ __  __ "
- echo -e "${BLUE}| || | /_\ \ / / _ \ / __| | __/ _ \| _ \   /_\ | _ \  \/  |"
- echo -e "${BLUE}| __ |/ _ \ V / (_) | (__  | _| (_) |   /  / _ \|   / |\/| |"
- echo -e "${BLUE}|_||_/_/ \_\_/ \___/ \___| |_| \___/|_|_\ /_/ \_\_|_\_|  |_|"
- echo -e "${BLUE}                                                            "
-
-echo -e "${GREEN} Created by KR45 (ezio)"
-
-echo -e "${BLUE} Checking packages"
-
-# amd64 package
-packages=("qemu-user-static" "binfmt-support")
-
-# Function to check if a package is installed
-check_package() {
-    dpkg -s "$1" &> /dev/null
-
-    if [ $? -eq 0 ]; then
-        echo -e "${GREEN}Package $1 is installed.${ENDCOLOR}"
-    else
-        echo -e "${RED}Package $1 is not installed.${ENDCOLOR}"
-        echo -e "${GREEN} Installing $1"
-        sudo apt install -y qemu-user-static binfmt-support
-    fi
+function print_blue () {
+    echo -e "${BLUE}${1}${ENDCOLOR}"
 }
 
-# Iterate through the list of packages and check each one
-for package in "${packages[@]}"
-do
-    check_package "$package"
-done
+function print_green () {
+    echo -e "${BLUE}${1}${ENDCOLOR}"
+}
 
-echo -e "${BLUE}Adding amd64 requirements"
+function print_red () {
+    echo -e "${BLUE}${1}${ENDCOLOR}"
+}
 
-if dpkg --print-foreign-architectures | grep -q "amd64"; then
-    echo -e "${GREEN}amd64 architecture successfully added.${ENDCOLOR}"
-else
-    echo -e "${RED}Adding amd64 architecture.${ENDCOLOR}"
-    sudo dpkg --add-architecture amd64
-    sudo apt update
-    echo -e "${GREEN}Adding Libraries"
-    sudo apt install libc6:amd64
+print_blue "_  _   ___   _____   ___   ___ ___  ___     _   ___ __  __ "
+print_blue "| || | /_\ \ / / _ \ / __| | __/ _ \| _ \   /_\ | _ \  \/  |"
+print_blue "| __ |/ _ \ V / (_) | (__  | _| (_) |   /  / _ \|   / |\/| |"
+print_blue "|_||_/_/ \_\_/ \___/ \___| |_| \___/|_|_\ /_/ \_\_|_\_|  |_|"
+print_blue "                                                            "
+
+print_green "Created by KR45 (ezio)"
+
+if [[ "${EUID}" -ne 0 ]];
+then
+    print_red "Please run as root"
     exit 1
 fi
 
-echo -e "${BLUE} HAVOC"
-
 #installing requried packages 
-
-hav=(
+print_blue "Checking packages"
+apt_packages=(
+    "qemu-user-static" 
+    "binfmt-support"
     "git"
     "build-essential"
     "apt-utils"
@@ -90,42 +71,34 @@ hav=(
     "bc"
 )
 
-check_package() {
-    dpkg -s "$1" &> /dev/null
+function check_package() {
+    dpkg -s "${1}" &> /dev/null
 
-    if [ $? -eq 0 ]; then
-        echo -e "${GREEN}Package $1 is already installed.${ENDCOLOR}"
+    if [ $? -eq 0 ]; 
+    then
+       print_green "Package ${1} is already installed."
     else
-        echo -e "${RED}Package $1 is not installed."
-        echo -e "${GREEN}Installing...${ENDCOLOR}"
-        sudo apt-get install -y "$1"
+        print_red "Package ${1} is not installed."
+        print_blue "Installing..."
+        sudo apt-get install -y "${1}"
     fi
 }
 
-echo -e "${BLUE}Checking package installations${ENDCOLOR}"
-
-# Iterate through the list of packages and check/install each one
-for package in "${hav[@]}"
-do
-    check_package "$package"
-done
-
-#checking python version
-
-install_pyenv() {
+function install_pyenv() {
     # Check if pyenv is already installed and configured in .zshrc
     if command -v pyenv >/dev/null 2>&1 && grep -q 'export PYENV_ROOT="$HOME/.pyenv"' ~/.zshrc; then
-        echo -e "${BLUE} pyenv is already installed and configured. Skipping installation."
+        print_blue "pyenv is already installed and configured. Skipping installation."
         return 0
     fi
 
-    echo -e "${BLUE} Installing pyenv and configuring shell."
+    print_blue "Installing pyenv and configuring shell."
 
     # Install pyenv using curl
-    curl https://pyenv.run | bash
+    curl -fsSL "https://pyenv.run" | bash
 
     # Add pyenv configuration to .zshrc if it doesn't already exist
-    if ! grep -q 'export PYENV_ROOT="$HOME/.pyenv"' ~/.zshrc; then
+    if [[ ! $(grep -q 'export PYENV_ROOT="$HOME/.pyenv"' ~/.zshrc) ]]; 
+    then
         echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
         echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
         echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init --path)"\nfi' >> ~/.zshrc
@@ -134,64 +107,38 @@ install_pyenv() {
         export PATH="$PYENV_ROOT/bin:$PATH"
         eval "$(pyenv init --path)"
     fi
-
 }
 
-py=$(python3 --version 2>&1 | awk '{print $2}')
-required_version="3.10"
-if command -v pyenv >/dev/null 2>&1; then
-    echo -e "${GREEN}pyenv is already installed. Skipping installation.${ENDCOLOR}"
-else
-    if [ "$py" = "$required_version" ]; then
-        echo -e "${GREEN}Python version is $required_version. Good to go!!!${ENDCOLOR}"
-    else
-        echo -e "${BLUE}Current Python version is $py_version.${ENDCOLOR}"
-        echo -e "${BLUE}Do you want to install Python 3.10? (y/n)${ENDCOLOR}"
-        read -r response
-        if [[ "$response" =~ ^[Yy]$ ]]; then
-            install_pyenv
-        else
-            echo -e "${RED}Python 3.10 installation skipped.${ENDCOLOR}"
-        fi
-    fi
-fi
-#clonning havoc
-# Clone Havoc repository if not already present
-if [ ! -d "Havoc" ]; then
-    git clone https://github.com/HavocFramework/Havoc.git
-fi
-
-
-
-# Check if the teamserver binary (or the target build file) already exists
-if [ ! -f "Havoc/havoc" ]; then
-    echo "Building teamserver binary..."
-    cd Havoc
+function build_teamserver_binary () {
     # Installing Havoc Go dependencies
     cd teamserver
+    go mod tidy
     go mod download golang.org/x/sys
     go mod download github.com/ugorji/go
     cd ..
     make ts-build
-else
-    echo "Teamserver binary already exists, skipping build."
-fi
-
-
-# Build the client binary
-build_client_binary() {
-    echo -e "${BLUE}Building the client binary...${ENDCOLOR}"
-    make client-build
-
-    if [ $? -eq 0 ]; then
-        echo -e "${GREEN}Client binary built successfully.${ENDCOLOR}"
+    if [ ${?} -eq 0 ]; 
+    then
+        print_green "Teamserver binary built successfully."
     else
-        echo -e "${RED}Error: Failed to build the client binary.${ENDCOLOR}"
+        print_blue "Teamserver binary already exists, skipping build."
     fi
 }
 
-# Function to check system requirements
-check_system_requirements() {
+# Build the client binary
+function build_client_binary () {
+    print_blue "Building the client binary..."
+    make client-build
+
+    if [ ${?} -eq 0 ]; then
+        print_green "Client binary built successfully."
+    else
+        print_red "Error: Failed to build the client binary."
+    fi
+}
+
+# Function to check system requirements and build binaries
+function check_system_requirements_and_build () {
     # Get the total amount of RAM in GB
     total_ram=$(grep MemTotal /proc/meminfo | awk '{print $2 / 1024 / 1024}')
 
@@ -199,30 +146,91 @@ check_system_requirements() {
     cpu_cores=$(nproc)
 
     # Print system information
-    echo -e "${BLUE}System Information:${ENDCOLOR}"
+    print_blue "System Information:"
     echo -e "Total RAM: ${total_ram} GB"
     echo -e "CPU Cores: ${cpu_cores}"
 
     # Check if RAM is less than 4 GB or CPU cores are less than 4
-if (( $(echo "$total_ram < 4" | bc -l) )) || [ "$cpu_cores" -lt 4 ]; then
-    if (( $(echo "$total_ram < 4" | bc -l) )); then
-        echo -e "${RED}Warning: RAM is less than 4 GB. Building may be slow.${ENDCOLOR}"
-    fi
+    if (( $(echo "$total_ram < 4" | bc -l) )) || [ "$cpu_cores" -lt 4 ]; 
+    then
+        if (( $(echo "$total_ram < 4" | bc -l) )); 
+        then
+            print_red "Warning: RAM is less than 4 GB. Building may be slow."
+        fi
 
-    if [ "$cpu_cores" -lt 4 ]; then
-        echo -e "${RED}Warning: Less than 4 CPU cores detected. Building may be slow.${ENDCOLOR}"
-    fi
+        if [ "$cpu_cores" -lt 4 ]; 
+        then
+            print_red "Warning: Less than 4 CPU cores detected. Building may be slow."
+        fi
 
-    # Build teamserver binary only if it doesn't exist
-    if [ ! -f "Havoc/client/Havoc" ]; then
-        echo "Building teamserver binary..."
-        build_client_binary
-    else
-        echo "Teamserver binary already exists, skipping build."
-    fi
-fi
+        print_blue " Adding amd64 requirements"
 
+        if dpkg --print-foreign-architectures | grep -q "amd64"; then
+            print_green "amd64 architecture successfully added."
+        else
+            print_red "Adding amd64 architecture."
+            sudo dpkg --add-architecture amd64
+            sudo apt update
+            print_green "Adding Libraries"
+            sudo apt install libc6:amd64
+            exit 1
+        fi
+
+        print_blue "Checking package installations"
+
+        # Iterate through the list of packages and check/install each one
+        for package in "${apt_packages[@]}";
+        do
+            check_package "${package}"
+        done
+
+        #checking python version
+        py=$(python3 --version 2>&1 | awk '{print $2}')
+        required_version="3.10"
+        if command -v pyenv >/dev/null 2>&1; 
+        then
+            print_green "pyenv is already installed. Skipping installation."
+        else
+            if [ "$py" = "$required_version" ]; 
+            then
+                print_green "Python version is $required_version. Good to go!!!"
+            else
+                print_blue "Current Python version is $py_version."
+                print_blue "Do you want to install Python 3.10? (y/n)"
+                read -r response
+                if [[ "$response" =~ ^[Yy]$ ]]; 
+                then
+                    install_pyenv
+                else
+                    print_red "Python 3.10 installation skipped."
+                fi
+            fi
+        fi
+
+        # Clone Havoc repository if not already present
+        if [ ! -d "Havoc" ]; 
+        then
+            git clone --recurse -b dev https://github.com/HavocFramework/Havoc.git Havoc
+        fi
+
+        cd Havoc
+
+        # Check if the teamserver binary (or the target build file) already exists
+        if [ ! -f "Havoc/havoc" ];
+        then
+            print_blue "Building teamserver binary..."
+        fi
+
+        # Build client binary only if it doesn't exist
+        if [ ! -f "Havoc/client/Havoc" ]; 
+        then
+            print_blue "Building client binary..."
+            build_client_binary
+        else
+            print_blue "Client binary already exists, skipping build."
+        fi
+    fi
 }
 
 # Check system requirements
-check_system_requirements
+check_system_requirements_and_build
